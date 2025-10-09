51b50821887cda720d99fc78b7ba6cdf434fbd63
RafaelSantana2007 <vrauzinmr2000@gmail.com>
2025-10-09 10:23:20 -0300

    .

---
+++ JavaScript/ex007/condicoes-parte2.js
@@ -0,0 +1,7 @@
+//CONDIÇÃO COMPOSTA!!!!!!!!!!!!
+var idade = 17
+if (idade >= 18) {
+    console.log(`Sua idade é de ${idade}, logo, já é maior de idade`)
+} else {
+    console.log(`A sua idade é de ${idade}, logo, você é menor de idade.`)
+}
---
+++ JavaScript/ex007/condicoes.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //condição simples
+    console.log('Você ultrapassou a velocidade permitida. CORNO!')
+}
+console.log('Dirija sempre com o cinto de segurança!')
