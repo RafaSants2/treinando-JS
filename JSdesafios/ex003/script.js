1d9fa5fe8cf119fc3cffc95cfc621b7843eb0685
RafaelSantana2007 <vrauzinmr2000@gmail.com>
2025-11-08 08:55:07 -0300

    .

--- JSdesafios/ex003/script.js
+++ JSdesafios/ex003/script.js
@@ -5,8 +5,27 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ) {
-        window.alert('[ERRO] Todavia faltam dados.')
+        res.innerHTML = 'Impossivel fazer a contagem!'
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerarei PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            //contagem crescente
+            for(let contador = i; contador <= f; contador += p) {
+            res.innerHTML += `${contador} \u{1F449}` /*esses códigos só funcionam entre crases*/
+            }
+        } else {
+            //contagem regressiva
+            for(let contador = i; contador >= f; contador -= p) { /*enquanto contador for menor ou igual ao fim, ele vai perder o passo*/
+                res.innerHTML += ` ${contador} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
