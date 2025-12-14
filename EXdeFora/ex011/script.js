476c6146df20f251a9fd1cc7a582c7c4af864191
RafaelSantana2007 <vrauzinmr2000@gmail.com>
2025-12-14 20:01:51 -0300

    .

--- EXdeFora/ex011/script.js
+++ EXdeFora/ex011/script.js
@@ -1,4 +1,5 @@
 /*quando eu clicar no botão, deve aparecer o menu, da mesma forma para removê-lo*/
+/*
 function descricao() {
     var texto = document.getElementById('relato')
     if (texto.style.display == 'none') {
@@ -6,4 +7,10 @@ function descricao() {
     } else {
         texto.style.display = 'none'
     }
+}
+*/
+
+function descricao() {
+    var texto = document.getElementById('relato')
+    texto.classList.toggle("js_desc")
 }
