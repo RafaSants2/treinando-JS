c198bc493a24430ead197e773d8da6d03f16d430
RafaelSantana2007 <vrauzinmr2000@gmail.com>
2025-12-14 10:23:03 -0300

    .

---
+++ FUNÇÕES/explicacao_e_exemplos.js
@@ -0,0 +1,11 @@
+/*function despedida(despedir) {
+    return 'Valeu! Boa noite para ti também.'
+}
+let resposta = despedida('boa noite')
+console.log(resposta)*/
+
+function multipli(a, b) {
+    return a * b
+}
+let resultado = multipli(5, 14)
+console.log(resultado)
